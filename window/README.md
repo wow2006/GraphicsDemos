91d53d23222d8a9c74d3526e8ae3ffa2c360553f
Ahmed Abdel Aal <eng.ahmedhussein89@gmail.com>
2020-01-11 14:52:32 +0200

    First Simple Window X11

--- window/README.md
+++ window/README.md
@@ -4,4 +4,5 @@ Creating Window
 - [Create SDL2 Window](simpleWindowSDL.cpp)
 - [Create GLFW3 Window](simpleWindowGLFW.cpp)
 - [Create SFML Window](simpleWindowSFML.cpp)
-
+- [Create X11 Window](simpleWindowX11.cpp)
+- [Create Win32 Window](simpleWindowWin32.cpp)
---
+++ window/simpleWindowX11.cpp
@@ -0,0 +1,59 @@
+// STL
+#include <optional>
+#include <cstdlib>
+#include <iostream>
+// X11
+#include <X11/Xlib.h>
+
+static auto parseProgramOptions(int argc, char** argv)
+  -> std::optional<std::pair<uint32_t, uint32_t>> {
+  if(argc != 3) {
+    return std::nullopt;
+  }
+
+  return std::make_pair(
+    std::stoi(argv[1]),
+    std::stoi(argv[2])
+  );
+}
+
+auto main(int argc, char *argv[]) -> int {
+  auto pDisplay = XOpenDisplay(nullptr);
+  if(pDisplay == nullptr) {
+    fprintf(stderr, "Cannot open display\n");
+    return EXIT_FAILURE;
+  }
+
+  auto width  = 640U;
+  auto height = 480U;
+  if(const auto args = parseProgramOptions(argc, argv);
+     args) {
+    constexpr auto primaryDisplay = 0;
+    auto screen = ScreenOfDisplay(pDisplay, primaryDisplay);
+    width  = std::min(args.value().first,  static_cast<uint32_t>(screen->width));
+    height = std::min(args.value().second, static_cast<uint32_t>(screen->height));
+  }
+
+  auto screen = DefaultScreen(pDisplay);
+  auto window = XCreateSimpleWindow(pDisplay,
+                RootWindow(pDisplay, screen),
+                0, 0, width, height, 1,
+                WhitePixel(pDisplay, screen),
+                BlackPixel(pDisplay, screen));
+  XSelectInput(pDisplay, window, ExposureMask | KeyPressMask);
+  XMapWindow(pDisplay, window);
+
+  std::cout << "Press any key to close the window!\n";
+
+  auto bRunning = true;
+  XEvent event;
+  while(bRunning) {
+    XNextEvent(pDisplay, &event);
+    if (event.type == KeyPress) {
+      bRunning = false;
+    }
+  }
+
+  XCloseDisplay(pDisplay);
+  return EXIT_SUCCESS;
+}
