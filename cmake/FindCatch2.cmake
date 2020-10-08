f9c2707daf54996bf3169e25c16c03f3f441c846
Ahmed Abdel Aal <eng.ahmedhussein89@gmail.com>
2020-10-08 18:47:47 +0200

    adding catch2

---
+++ cmake/FindCatch2.cmake
@@ -0,0 +1,25 @@
+# ${CMAKE_SOURCE_DIR}/cmake/FindCatch2.cmake
+if(TARGET CATCH2::header_only)
+  return()
+endif()
+
+set(CATCH_DIR ${CMAKE_SOURCE_DIR}/thirdparty/Catch2/)
+if(NOT EXISTS ${CATCH_DIR}/include/catch2/catch.hpp)
+  file(MAKE_DIRECTORY ${CATCH_DIR}/include/catch2/)
+  file(
+    DOWNLOAD https://github.com/catchorg/Catch2/releases/download/v2.13.1/catch.hpp
+    ${CATCH_DIR}/include/catch2/catch.hpp
+  )
+endif()
+
+add_library(
+  CATCH2::header_only
+  INTERFACE IMPORTED
+)
+
+set_target_properties(
+  CATCH2::header_only
+  PROPERTIES
+  INTERFACE_INCLUDE_DIRECTORIES ${CATCH_DIR}/include/
+)
+
