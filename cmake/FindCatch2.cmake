97945c162f99a8fa60f445b118173b84481fec5e
Ahmed Abdel Aal <eng.ahmedhussein89@gmail.com>
2020-10-08 20:30:28 +0200

    create catch main

--- cmake/FindCatch2.cmake
+++ cmake/FindCatch2.cmake
@@ -6,6 +6,7 @@ endif()
 set(CATCH_DIR ${CMAKE_SOURCE_DIR}/thirdparty/Catch2/)
 if(NOT EXISTS ${CATCH_DIR}/include/catch2/catch.hpp)
   file(MAKE_DIRECTORY ${CATCH_DIR}/include/catch2/)
+  file(WRITE "// Catch2\n#define CATCH_CONFIG_MAIN\n#include <catch2/catch.hpp>\n" ${CATCH_DIR}/catch.cpp)
   file(
     DOWNLOAD https://github.com/catchorg/Catch2/releases/download/v2.13.1/catch.hpp
     ${CATCH_DIR}/include/catch2/catch.hpp
