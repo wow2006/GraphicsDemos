f8dfff62087c17056b71efe09b86bb978491dca9
Ahmed Abdel Aal <eng.ahmedhussein89@gmail.com>
2020-10-24 18:31:00 -0700

    Finish fix pipeline OpenGL Win32

--- opengl/fixedPipelineOpenGLWin32.cpp
+++ opengl/fixedPipelineOpenGLWin32.cpp
@@ -2,11 +2,72 @@
 #include <string_view>
 // OS
 #include <Windows.h>
+// OpenGL
+#include <gl/GL.h>
+#include <gl/GLU.h>
 
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+constexpr float FovY = 45.0F;
+constexpr float Near = 3.0F;
+constexpr float Far  = 7.0F;
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, PSTR pCmdLine, int nCmdShow) {
+void resize(HWND hWnd) {
+  RECT rect = {};
+  GetClientRect(hWnd, &rect);
+  const GLsizei width = rect.right - rect.left;
+  const GLsizei height = rect.bottom - rect.top;
+  glViewport(0, 0, width, height);
+}
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+  switch(uMsg) {
+  case WM_CLOSE:
+    DestroyWindow(hWnd);
+    return 0;
+    case WM_DESTROY:
+    PostQuitMessage(0);
+    return 0;
+  case WM_SIZE:
+      resize(hWnd);
+      break; 
+  }
+  return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
+
+bool SetupPixelFormat(HDC hdc) {
+  PIXELFORMATDESCRIPTOR pfd, *ppfd;
+  int pixelformat;
+
+  ppfd = &pfd;
+
+  // clang-format off
+  ppfd->nSize = sizeof(PIXELFORMATDESCRIPTOR);
+  ppfd->nVersion     = 1;
+  ppfd->dwFlags      = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+  ppfd->dwLayerMask  = PFD_MAIN_PLANE;
+  ppfd->iPixelType   = PFD_TYPE_COLORINDEX;
+  ppfd->cColorBits   = 8;
+  ppfd->cDepthBits   = 16;
+  ppfd->cAccumBits   = 0;
+  ppfd->cStencilBits = 0;
+  // clang-format on
+
+  pixelformat = ChoosePixelFormat(hdc, ppfd);
+
+  if((pixelformat = ChoosePixelFormat(hdc, ppfd)) == 0) {
+    MessageBox(nullptr, "ChoosePixelFormat failed", "Error", MB_OK);
+    return false;
+  }
+
+  if(SetPixelFormat(hdc, pixelformat, ppfd) == FALSE) {
+    MessageBox(nullptr, "SetPixelFormat failed", "Error", MB_OK);
+    return false;
+  }
+
+  return true;
+} 
+
+HWND CreateMainWindow(HINSTANCE hInstance, int nCmdShow) {
   int width = 640;
   int height = 480;
 
@@ -14,13 +75,15 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, PSTR pCmdLine, int nCmdShow) 
 
   WNDCLASS windowClass = {};
 
-  windowClass.lpfnWndProc = WindowProc;
-  windowClass.hInstance = hInstance;
+  // clang-format off
+  windowClass.lpfnWndProc   = WindowProc;
+  windowClass.hInstance     = hInstance;
   windowClass.lpszClassName = cTitle.data();
+  // clang-format on
 
   RegisterClass(&windowClass);
 
-  HWND hwnd = CreateWindowEx(0,
+  HWND hWnd = CreateWindowEx(0,
                              cTitle.data(),
                              cTitle.data(),
                              WS_OVERLAPPEDWINDOW,
@@ -34,31 +97,69 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, PSTR pCmdLine, int nCmdShow) 
                              nullptr     // Additional application data
   );
 
-  if(hwnd == nullptr) {
+  if(hWnd == nullptr) {
     OutputDebugString("Can not create win32 Window");
-    return 0;
+    return nullptr;
   }
 
-  ShowWindow(hwnd, nCmdShow);
-
-  MSG msg = {};
-  while(GetMessage(&msg, nullptr, 0, 0)) {
-    TranslateMessage(&msg);
-    DispatchMessage(&msg);
+  ShowWindow(hWnd, nCmdShow);
+  return hWnd;
+}
+std::pair<HGLRC, HDC> CreateOpenGLContext(HWND hWnd) {
+  const auto hDC = GetDC(hWnd);
+  if(!SetupPixelFormat(hDC)) {
+    PostQuitMessage(0);
   }
 
-  return 0;
+  const HGLRC hRC = wglCreateContext(hDC);
+  wglMakeCurrent(hDC, hRC);
+  return {hRC, hDC};
+}
+void InitializeOpenGL(HWND hWnd) {
+  resize(hWnd);
+  glClearColor(1, 0, 0, 1);
+}
+void Draw(HDC hDC) {
+  glClear(GL_COLOR_BUFFER_BIT);
+  glBegin(GL_QUADS);
+      glVertex3f( 0.5f, 0.5f, 0.0f);
+      glVertex3f( 0.5f,-0.5f, 0.0f);
+      glVertex3f(-0.5f,-0.5f, 0.0f);
+      glVertex3f(-0.5f, 0.5f, 0.0f);
+  glEnd();
+  SwapBuffers(hDC);
+}
+void MainLoop(HDC hDC) {
+  bool bRunning = true;
+  while(bRunning) {
+    MSG message = {};
+    while(PeekMessage(&message, nullptr, 0, 0, PM_NOREMOVE) == TRUE) {
+      if(GetMessage(&message, nullptr, 0, 0)) {
+        TranslateMessage(&message);
+        DispatchMessage(&message);
+      } else {
+        bRunning = false;
+        break;
+      }
+    }
+    Draw(hDC);
+  }
+}
+void Cleanup(HWND hWindow, HDC hDC, HGLRC hRC) {
+  wglDeleteContext(hRC);
+  ReleaseDC(hWindow, hDC);
+  DestroyWindow(hWindow);
 }
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-  switch(uMsg) {
-  case WM_DESTROY: {
-    PostQuitMessage(0);
-    return 0;
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR pCmdLine, int nCmdShow) {
+  const auto hWindow = CreateMainWindow(hInstance, nCmdShow);
+  if(hWindow == nullptr) {
+      return 1;
   }
-  case WM_PAINT: {
-    return 0;
-  }
-  }
-  return DefWindowProc(hwnd, uMsg, wParam, lParam);
+  const auto [hRC, hDC] = CreateOpenGLContext(hWindow);
+  InitializeOpenGL(hWindow);
+  MainLoop(hDC);
+  Cleanup(hWindow, hDC, hRC);
+
+  return 0;
 }
